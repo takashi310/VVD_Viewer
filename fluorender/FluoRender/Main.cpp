92ed02331af886d5f8adeec466baa8e2bd03e16b
takashi310 <ymrnztm310@gmail.com>
2023-02-23 14:04:33 -0500

    update the version number

--- fluorender/FluoRender/Main.cpp
+++ fluorender/FluoRender/Main.cpp
@@ -68,7 +68,7 @@ bool VRenderApp::OnInit()
       std::string(VERSION_MAJOR_TAG) +  std::string(".") +
       std::string(VERSION_MINOR_TAG);
 */   
-   std::string title =  std::string(FLUORENDER_TITLE) + "1.5.11";
+   std::string title =  std::string(FLUORENDER_TITLE) + "1.5.12";
 
    m_frame = new VRenderFrame(
 	     this,
